07b9a35afdcc0d6fc1e2542b4348acff5ea8b82a
Gustavo Zapata <tavordie@hotmail.com>
2019-06-21 18:30:40 +0100

    first commit from workstation

---
+++ src/main/java/me/gustavozapata/me/GzSpringBootApplication.java
@@ -0,0 +1,13 @@
+package me.gustavozapata.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GzSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GzSpringBootApplication.class, args);
+    }
+
+}
