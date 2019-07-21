9898bef1e1d3ae1880bb9c5bb16dcdb549969555
Gustavo Zapata <tavordie@hotmail.com>
2019-07-21 21:56:14 +0100

    implement the backend to accept the api requests

--- src/main/java/me/gustavozapata/me/controller/ApiController.java
+++ src/main/java/me/gustavozapata/me/controller/ApiController.java
@@ -2,12 +2,59 @@ package me.gustavozapata.me.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.*;
 
 @Controller
 public class ApiController {
 
+    ArrayList<String> keys = new ArrayList<>(Arrays.asList("u7asdfkjc23", "ht234jkddfs", "adasfdf4g34", "4k418w9f17b"));
+
     @GetMapping("/api")
     public String api(){
         return "api.html";
+    }
+
+    @GetMapping("/api/news")
+    @ResponseBody
+    public String news(){
+        return "api.news[{id:1, heading: vamonos pest, image: url}, {id:2, heading: cali, image: url2}]";
+    }
+
+    @GetMapping("/api/weather")
+    @ResponseBody
+    public String weather(){
+        return "api.weather[{id:1, location: sf, temperature: 35}, {id:2, location: cali, temperature: 40}]";
+    }
+
+    @RequestMapping(value = "/api/currency/{key}", produces = "application/json")
+    @ResponseBody
+    public Map<Map, Map> currency(@PathVariable("key") String key){
+        Map<Map, Map> map = new HashMap();
+        if(keys.contains(key)){
+            Map<String, String> map1 = new HashMap();
+            map1.put("id", "1");
+            map1.put("country", "col");
+            map1.put("value", "15");
+            Map<String, String> map2 = new HashMap();
+            map2.put("id", "1");
+            map2.put("country", "uk");
+            map2.put("value", "157");
+            Map<String, String> map3 = new HashMap();
+            map3.put("id", "2");
+            map3.put("country", "esp");
+            map3.put("value", "135");
+            map.put(map1, map2);
+//            return "api.currency[{id:1, country: col, value: 35}, {id:2, country: usa, value: 40}]";
+//            return "[{'id':1, 'country':'col', 'value': 28},{'id':2, 'country':'usa', 'value': 34},{'id':3, 'country':'esp', 'value': 29}]";
+//            return Collections.singletonMap("api","private-currency");
+            return map;
+        }
+//        return "error: invalid key";
+//        return Collections.singletonMap("error","invalid key");
+        return map;
     }
 }
--- src/main/webapp/scripts/api.js
+++ src/main/webapp/scripts/api.js
@@ -1,28 +1,77 @@
+var hasKeyGenerated = false;
+var keyInterval;
+var url = "";
+var request = "";
+
 $(".category button").on("click", function(e) {
+  buildUrl(e);
   $(".category button").removeClass("category-selected");
   $(this).toggleClass("category-selected");
-  if (e.target.id === "private") {
-    $(".generate-key").toggleClass("generate-key-active");
+  if (e.target.id === "currency") {
+    $(".generate-key").addClass("generate-key-active");
+    if (hasKeyGenerated) {
+      $(".loading").css("visibility", "visible");
+    }
   } else {
     $(".generate-key").removeClass("generate-key-active");
     $(".loading").css("visibility", "hidden");
   }
 });
 
-$(".requests button").on("click", function() {
+function buildUrl(e) {
+  url = "/api/" + e.target.id;
+  $(".input-text").val(url);
+}
+
+$(".requests button").on("click", function(e) {
   $(".requests button").removeClass("request-selected");
   $(this).toggleClass("request-selected");
+  request = e.target.id;
 });
 
 $(".generate-key").on("click", function() {
-  $(".loading").css("visibility", "visible");
-  setInterval(function() {
-    loading();
-  }, 5000);
+  $(this).css("pointer-events", "none");
+  if (!hasKeyGenerated) {
+    $(".loading").css("visibility", "visible");
+    hasKeyGenerated = true;
+    var key = generateKey();
+    key = "u7asdfkjc23";
+    keyInterval = setInterval(function() {
+      loaded(key);
+    }, 2000);
+  }
 });
 
-function loading() {
+function loaded(key) {
   $(".loading p:first-child").text("a key has been generated");
   $(".loading img").css("display", "none");
-  $("#key").css("display", "block");
+  $("#key")
+    .css("display", "block")
+    .text(key);
+  $(".input-text").val((url += "/" + key));
+  clearInterval(keyInterval);
+}
+
+function generateKey() {
+  var key = Math.random()
+    .toString(36)
+    .substr(2, 12);
+  return key;
+}
+
+$("#send").on("click", function() {
+  if (request === "get") {
+    $.get(url, function(result) {
+      $(".response textarea").val(JSON.stringify(result));
+    });
+  }
+});
+
+//a more aggresive unique id
+function uuidv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
 }
